3119c187e92a7240657eb364477bcb2b62f7be74
Semicomatose <gloomeria@gmail.com>
2026-05-19 20:08:02 +0700

    feat: added main.pl

---
+++ src/main.pl
@@ -0,0 +1,7 @@
+:- include('fakta.pl').
+:- include('rules.pl').
+:- include('startGame.pl').
+:- include('mainkanKartu.pl').
+:- include('uni.pl').
+:- include('command.pl').
+:- include('endGame.pl'). 
